608d66a4c321a4768c862c21241e0f50a4c5e702
Soniya <boralakuntasoniya@gmail.com>
2024-10-25 12:10:30 +0530

    project

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('login-btn').addEventListener('click', function () {
+    document.getElementById('login-page').style.display = 'none';
+    document.getElementById('home-design-page').style.display = 'block';
+});
